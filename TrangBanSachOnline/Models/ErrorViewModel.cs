7d705fd5c4ea5f864e207a2b0dd12442236a715a
Viet382004 <viet01386@gmail.com>
2025-10-30 22:26:01 +0700

    Khoi tao du an

---
+++ TrangBanSachOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TrangBanSachOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
